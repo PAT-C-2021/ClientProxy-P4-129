96018e33eb8d58666d19b059f4c2b2f1997aa37c
rafiffuaddo <rafiffuad3108@gmail.com>
2021-11-07 22:40:44 +0700

    Add project files.

---
+++ ClientProxy-P4-20190140129/ClientProxy_P4_20190140129/ServiceReference1/Koordinat.cs
@@ -0,0 +1,8 @@
+﻿namespace ClientProxy_P4_20190140129.ServiceReference1
+{
+    internal class Koordinat
+    {
+        public int Y { get; internal set; }
+        public int X { get; internal set; }
+    }
+}
